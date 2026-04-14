34de4c57259cde93257768090ce7bd4d8d20d39b
Phillip Alday <me@phillipalday.com>
2026-04-13 21:47:22 -0600

    delete stubs

--- src/OndaVision.jl
+++ src/OndaVision.jl
@@ -4,8 +4,6 @@ using Onda
 
 export read_vhdr, parse_amplifier_setup, parse_software_filters, parse_impedances
 
-greet() = print("Hello World!")
-
 include("read_vhdr.jl")
 include("read_vmrk.jl")
 
