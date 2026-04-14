29d242056cf3da54462b504b7cd3d926af9cdf9a
Phillip Alday <me@phillipalday.com>
2026-04-13 19:39:12 -0600

    stub and a note

--- src/OndaVision.jl
+++ src/OndaVision.jl
@@ -4,4 +4,25 @@ using Onda
 
 greet() = print("Hello World!")
 
+"""
+    read_vhdr(filename)
+    read_vhdr(io::IO)
+
+Returns a nested dictionary of configuration entries from a BrainVision VHDR file.
+
+`filename` may be any entity with an appropriate `open` method.
+
+The BrainVision VHDR format is similar to the Windows INI configuration format, but
+has a few additional extensions.
+"""
+function read_vhdr(filename; kwargs...)
+    return open(filename, "r") do io
+        return read_vhdr(io, kwargs...)
+    end
+end
+
+
+function read_vhdr(io::IO)
+end
+
 end # module OndaVision
