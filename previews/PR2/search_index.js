195cdcd96a464e229c478d247cbebfde0c8f608e
Documenter.jl <documenter@juliadocs.github.io>
2026-04-22 07:20:44 +0000

    delete history

---
+++ previews/PR2/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"category":"section","location":"api/#API","page":"API","text":"","title":"API"},{"category":"section","location":"#OndaVision.jl-Documentation","page":"OndaVision.jl Documentation","text":"OndaVision.jl is a Julia package that provides support for reading and writing BrainVision files as Onda data.\n\nPages = [\n        \"api.md\",\n]\nDepth = 1","title":"OndaVision.jl Documentation"}]
+}
